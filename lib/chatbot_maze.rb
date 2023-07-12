bf7774856b1a8ff0bced7e985ec1c042f4d04cd6
PaulO6967 <p.osswald67@gmail.com>
2023-07-12 17:34:44 +0200

    advanced chat bot

--- lib/chatbot_maze.rb
+++ lib/chatbot_maze.rb
@@ -4,24 +4,38 @@ require 'dotenv'
 
 Dotenv.load('.env')
 
-api_key = ENV["OPENAI_API_KEY"]
-url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+$api_key = ENV["OPENAI_API_KEY"]
+$url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
 
-headers = {
+$headers = {
   "Content-Type" => "application/json",
-  "Authorization" => "Bearer #{api_key}"
+  "Authorization" => "Bearer #{$api_key}"
 }
 
-data = {
-  "prompt" => conversation_history,
-  "max_tokens" => 150,
-  "n" => 1,
-  "temperature" => 0.0
-}
+def user_prompt(conversation_history)
+  user_prompt = ""
+  until user_prompt == "bye" do
+    print "Vous> "
+    user_prompt = gets.chomp
+    conversation_history << "Vous> #{user_prompt}"
+    converse_with_ai(user_prompt, conversation_history)
+  end
+puts conversation_history
+end
 
-response = HTTP.post(url, headers: headers, body: data.to_json)
-response_body = JSON.parse(response.body.to_s)
-response_string = response_body['choices'][0]['text'].strip
+def converse_with_ai(user_prompt, conversation_history)
+  data = {
+    "prompt" => user_prompt,
+    "max_tokens" => 150,
+    "n" => 1,
+    "temperature" => 0.0
+  }
 
-puts "Voici 5 parfums de glace :"
-puts response_string
+  response = HTTP.post($url, headers: $headers, body: data.to_json)
+  response_body = JSON.parse(response.body.to_s)
+  response_string = response_body['choices'][0]['text'].strip
+  conversation_history << "IA> #{response_string}"
+  puts "IA> #{response_string}"
+end
+
+user_prompt([])
